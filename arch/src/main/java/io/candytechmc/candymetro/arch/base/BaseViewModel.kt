29f9e168e48b01043530efda203fc36567b41a49
Sokeriaaa <lovelysokeriaaa@outlook.com>
2023-01-21 10:27:19 +0800

    BaseViewModel.kt添加KoinComponent接口

--- arch/src/main/java/io/candytechmc/candymetro/arch/base/BaseViewModel.kt
+++ arch/src/main/java/io/candytechmc/candymetro/arch/base/BaseViewModel.kt
@@ -4,13 +4,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import org.koin.core.component.KoinComponent
 
 /**
  * Base ViewModel
  * @author Sokeriaaa
  * @date 2023/1/17
  */
-abstract class BaseViewModel : ViewModel() {
+abstract class BaseViewModel : ViewModel(), KoinComponent {
 
     var isLoading by mutableStateOf(false)
 
